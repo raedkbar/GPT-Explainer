f03b1b05349ea3bf4de38577dc9cb01214cac74a
Raed Barhoum <96176008+raedkbar@users.noreply.github.com>
2023-06-28 23:24:35 +0300

    Upgraded flaskWebAPI.py

--- src/flaskWebAPI.py
+++ src/flaskWebAPI.py
@@ -1,11 +1,12 @@
-import glob
 import os
 import json
 import uuid
-from datetime import datetime
+
 from flask import Flask, request, jsonify
+from datetime import datetime
 from werkzeug.utils import secure_filename
-from pathlib import Path
+from src.db_session import create_session
+from src.models import Upload, User
 
 app = Flask(__name__)
 UPLOADS_DIR = "./uploads"
@@ -15,14 +16,13 @@ OUTPUTS_DIR = "./outputs"
 os.makedirs(UPLOADS_DIR, exist_ok=True)
 os.makedirs(OUTPUTS_DIR, exist_ok=True)
 
+session = create_session()
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
     """
     Uploads a file to the server.
-
-    Args:
-        None (request is accessed directly)
 
     Returns:
         JSON response with UID if successful, error message otherwise
@@ -39,66 +39,85 @@ def upload_file():
     uid = str(uuid.uuid4())
 
     # Create timestamp for the upload
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    timestamp = datetime.now()
 
     # Create the filename with original filename, timestamp, and UID
     filename = secure_filename(file.filename)
-    name, extension = filename.rsplit('.', 1)
-    new_filename = f"{name}_{timestamp}_{uid}.{extension}"
 
     # Save the file to the uploads folder
-    file.save(os.path.join(UPLOADS_DIR, new_filename))
+    file.save(os.path.join(UPLOADS_DIR, f"{uid}.pptx"))
+
+    email = request.form.get("email")
+    user = None
+
+    if email:
+        # Query the User table for the user with the specified email
+        user = session.query(User).filter_by(email=email).first()
+        if not user:
+            # User not found, create a new user
+            user = User(email=email)
+            session.add(user)
+            session.commit()
+
+    # Create an Upload object and commit it to the database
+    upload = Upload(uid=uid, filename=filename, upload_time=timestamp, status="pending", user=user)
+    session.add(upload)
+    session.commit()
 
     return jsonify({"uid": uid}), 200
 
 
-@app.route("/status/<uid>", methods=["GET"])
-def get_status(uid):
+@app.route("/status", methods=["GET"])
+def get_status():
     """
     Retrieves the status of an upload.
-
-    Args:
-        uid (str): The unique identifier for the upload
 
     Returns:
         JSON response with the status, filename, timestamp, and explanation
     """
-    # Check if the upload exists
-    upload_path = os.path.join(UPLOADS_DIR, f"*_{uid}.*")
-    matching_uploads = glob.glob(upload_path)
+    uid = request.args.get('uid')
+    filename = request.args.get('filename')
+    email = request.args.get('email')
+    upload = None
 
-    if len(matching_uploads) == 0:
-        return jsonify({"status": "not found"}), 404
+    if uid:
+        upload = session.query(Upload).filter_by(uid=uid).first()
+    elif filename and email:
+        user = session.query(User).filter_by(email=email).first()
+        upload = (
+            session.query(Upload)
+            .filter_by(filename=filename, user=user)
+            .order_by(Upload.upload_time.desc())
+            .first()
+        )
 
-    upload_filename = os.path.basename(matching_uploads[0])
-    timestamp = upload_filename.split("_")[-2]
+    if not upload:
+        return jsonify({'error': 'Upload not found'}), 404
 
-    stripped_string = upload_filename.split("_", 1)[1]
+    # Retrieve the status, filename, and timestamp
+    status = upload.status
+    filename = upload.filename
+    timestamp = upload.upload_time
 
-    output_path = Path(OUTPUTS_DIR) / f"{stripped_string.split('.')[0]}.json"
-    output_path = output_path.resolve()
-
-    # Check if the output file exists
-    if output_path.exists():
-        with open(output_path, "r") as f:
-            output_data = json.load(f)
-
-        # Extract all slides explanation
-        explanation = ''.join(slide["explanation"] for slide in output_data)
-
-        return jsonify({
-            "status": "done",
-            "filename": upload_filename,
-            "timestamp": timestamp,
-            "explanation": explanation
-        })
+    # Check if the upload has finished processing
+    explanation = ""
+    if status == "completed":
+        explanation_file = os.path.join(OUTPUTS_DIR, f"{uid}.json")
+        if os.path.exists(explanation_file):
+            with open(explanation_file, "r") as f:
+                explanation_data = json.load(f)
+            explanation = '\n'.join(slide['explanation'] for slide in explanation_data)
+    elif status == "failed":
+        explanation = "Processing failed"
+    else:
+        explanation = "Processing in progress"
 
     return jsonify({
-        "status": "pending",
-        "filename": upload_filename,
+        "status": status,
+        "filename": filename,
         "timestamp": timestamp,
-        "explanation": None
-    })
+        "explanation": explanation
+    }), 200
 
 
 if __name__ == "__main__":
