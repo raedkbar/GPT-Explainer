2d6a876ff81aab7090e620e0d207d824380616f0
Raed Barhoum <96176008+raedkbar@users.noreply.github.com>
2023-07-10 17:56:38 +0300

    Update gpt_explainer.py
    Instead of printing multiple "Scanning pending uploads", now it's printed only once, followed by 3 dots.

--- src/gpt_explainer.py
+++ src/gpt_explainer.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from pkg.db_util.db_session import create_session
 from pkg.db_util.ORM import Upload
@@ -10,20 +11,21 @@ session = create_session()
 async def process_uploads():
     """
     Processes the pending uploads and generates explanations for each PowerPoint presentation.
-
     The function runs indefinitely, scanning the database for pending uploads and processing them.
     """
+    message = "Scanning pending uploads"
+    dot_count = 0
     while True:
-        print("Scanning pending uploads...\n")
+        dot_count = (dot_count + 1) % 4
+        time.sleep(0.5)
+        dots = "." * dot_count + " " * (3 - dot_count)
+        print(f"\r{message}{dots}", end="")
 
         pending_uploads = session.query(Upload).filter_by(status="pending").all()
 
         for upload in pending_uploads:
             upload_id = upload.id
             await process_presentation(upload_id)
-
-        print("Sleeping for 10 seconds...\n")
-        await asyncio.sleep(10)
 
 
 if __name__ == "__main__":
